49d5c126e002a08d51ac463fe496827c9612c356
glaubercsouza <glauber_costa14@yahoo.com.br>
2021-07-27 23:09:06 -0300

    pessoa

--- estudos/Agenda/src/Dados/Pessoa.java
+++ estudos/Agenda/src/Dados/Pessoa.java
@@ -1,5 +1,38 @@
+/*
+    * To change this template, choose Tools | Templates
+    * and open the template in the editor.
+*/
 package Dados;
 
+
 public class Pessoa {
+    String  nome;
+    String  telefone;
+    int     idade;
     
+        public Pessoa(){
+        idade = 0;
+        nome = "";
+        telefone = "";
+    }
+        
+        public Pessoa(String nom, String tel, int id)
+        {
+            nome = nom;
+            telefone = tel;
+            idade = id;
+        }    
+        
+        public boolean testeIdade(int idade)
+        {
+            if(idade >= 0)
+                return true;
+            else
+                return false;
+        }
+        
+        public String mostrarDados(){
+            String retorno = "Nome: " + nome + " Idade: " + idade + " Telefone: "+ telefone;
+            return retorno;
+        }
 }
