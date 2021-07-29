da8dbeccb26efd9367d50be7112081fc08abb779
glaubercsouza <glauber_costa14@yahoo.com.br>
2021-07-28 21:25:24 -0300

    agenda

--- estudos/Agenda/src/Dados/Pessoa.java
+++ estudos/Agenda/src/Dados/Pessoa.java
@@ -4,35 +4,68 @@
 */
 package Dados;
 
-
 public class Pessoa {
-    public String  nome;
-    public String  telefone;
-    public int     idade;
+    private String  nome;
+    private String  telefone;
+    private int     idade;
+
+    public int getIdade(){
+        return idade;
+    }
     
-        public Pessoa(){
-        idade = 0;
-        nome = "";
-        telefone = "";
+    public void setIdade(int idade){
+        if(testeIdade(idade))
+            this.idade = idade;
+        else
+            this.idade = 0;
+    }
+    
+    public String getNome(){
+        return nome;
+    }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public String getTelefone(){
+        return telefone;
+    }
+    
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
     }
         
-        public Pessoa(String nom, String tel, int id)
-        {
-            nome = nom;
-            telefone = tel;
-            idade = id;
-        }    
+    public Pessoa(){
+        this.setIdade(0);
+        this.setNome("");
+        this.setTelefone("");
+    }
         
-        public boolean testeIdade(int idade)
-        {
-            if(idade >= 0)
-                return true;
-            else
-                return false;
-        }
+    public Pessoa(String nom, String tel, int id)
+    {
+        this.setIdade(id);
+        this.setTelefone(tel);
+        this.setNome(nom);
+    }    
         
-        public String mostrarDados(){
-            String retorno = "Nome: " + nome + " Idade: " + idade + " Telefone: "+ telefone;
-            return retorno;
-        }
+    public boolean testeIdade(int idade)
+    {
+        if(idade >= 0)
+            return true;
+        else
+        return false;
+    }
+        
+    public String mostrarDados(){
+        String retorno = "Nome: " + nome + " Idade: " + idade + " Telefone: "+ telefone;
+        return retorno;
+    }
+        
+    public Pessoa(Pessoa nova)
+    {
+        this.nome = nova.nome;
+        this.idade = nova.idade;
+        this.telefone = nova.telefone;
+    }
 }
--- estudos/Agenda/src/Principal/Inicio.java
+++ estudos/Agenda/src/Principal/Inicio.java
@@ -6,9 +6,9 @@ public class Inicio {
     public static void main(String[] args) {
         Pessoa pes = new Pessoa();
         Pessoa pes2 = new Pessoa("João", "(11) 0980-0980", 13);
-        pes.nome = "Fulano";
-        pes.idade = 22;
-        pes.telefone = "(00) 7654-3210";
+        pes.setNome("Fulano");
+        pes.setIdade(22);
+        pes.setTelefone("(00) 7654-3210");
         System.out.println(pes.mostrarDados());
         System.out.println(pes2.mostrarDados());
     }
