b1d77f9ade09d6e1be9fe3b5fc111275319b21c6
glaubercsouza <glauber_costa14@yahoo.com.br>
2021-09-12 13:57:25 -0300

    arrays ex02

---
+++ ExemplosArray/src/Arrays/Exemplo02.java
@@ -0,0 +1,29 @@
+
+package Arrays;
+
+import javax.swing.*;
+
+
+public class Exemplo02 {
+    public static void main(String[] args) {
+        // cria um array com 6 elementos
+        int[] array = {10, 15, 25, 3, 8, 7};
+        int total = 0;
+        String saida = "Índice\tValor\n";
+        
+        // adicionando o valor de cada elemento do Array na variável saída
+        for(int i = 0; i < array.length; i++) {
+            total += array[i];
+            saida += i + "\t" + array[i] + "\n";
+        }
+        saida += "\n\nA soma dos elementos do array é: " + total;
+        
+        JTextArea saidaArea = new JTextArea();
+        saidaArea.setText(saida);
+        
+        JOptionPane.showMessageDialog(null, saidaArea, 
+                "Criando um Array de Inteiros", JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+        
+    }
+}
