bfac1319c69a02505c89d1cde41d000499d784e6
glaubercsouza <glauber_costa14@yahoo.com.br>
2021-09-14 22:53:31 -0300

    ex06

--- ExemplosArray/src/ArraysAndCollections/Exemplo05.java
+++ ExemplosArray/src/ArraysAndCollections/Exemplo05.java
@@ -1,14 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ArraysAndCollections;
 
-/**
- *
- * @author User
- */
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+
+
 public class Exemplo05 {
-    
+    public static void main(String[] args) {
+        List<String> listaNomes = new ArrayList<String>();
+        listaNomes.add("Maria");
+        listaNomes.add("João");
+        listaNomes.add("José");
+        listaNomes.add("Adriano");
+        listaNomes.add("Willian");
+        listaNomes.add("Patrícia");
+        
+        String saida = "";
+        saida += "Lista desordenada: " + listaNomes;
+        
+        Collections.sort(listaNomes);
+        saida += "\nLista ordenada: " + listaNomes;
+        
+        JTextArea saidaArea = new JTextArea();
+        saidaArea.setText(saida);
+        
+        JOptionPane.showMessageDialog(null, saidaArea, "Trabalhando com ArrayList", JOptionPane.INFORMATION_MESSAGE);
+        
+        System.exit(0);
+    }
 }
