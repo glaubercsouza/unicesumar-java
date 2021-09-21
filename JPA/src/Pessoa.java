486cfdcc5d05ed2c2b4ed085908b125b182eb714
glaubercsouza <glauber_costa14@yahoo.com.br>
2021-09-20 21:28:42 -0300

    db

---
+++ JPA/src/Pessoa.java
@@ -0,0 +1,11 @@
+import javax.persistence.Entity;
+9 import javax.persistence.GeneratedValue;
+10 import javax.persistence.GenerationType;
+11 import javax.persistence.Id;
+12 import javax.persistence.Temporal;
+13 import javax.persistence.TemporalType;
+14 import javax.persistence.Transient;
+@Entity        
+public class Pessoa {
+    
+}
