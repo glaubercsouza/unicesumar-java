7f3df89d45ad8771cbe21f636ddde1588ded26cc
Glauber Souza <glauber_costa14@yahoo.com.br>
2022-07-24 16:45:05 -0300

    caixa eletronico

--- Programacao III/CaixaEletronico/src/caixaeletronico/CaixaEletronico.java
+++ Programacao III/CaixaEletronico/src/caixaeletronico/CaixaEletronico.java
@@ -1,13 +1,55 @@
 package caixaeletronico;
 
 import cliente.Fisica;
+import cliente.Juridica;
+import cliente.Pessoa;
+
+import java.util.Scanner;
 
 public class CaixaEletronico {
 
+    static Pessoa p[] = new Pessoa[10];
+    public static int ultimo = 0;
+    
     public static void main(String[] args) {
-        Fisica f = new Fisica();
-        f.cadastra();
-        System.out.println(f.getNomePessoa());
+        Scanner tec = new Scanner(System.in);
+        int opcao = 0;
+        while(opcao != 4) {
+            System.out.println("Digite: ");
+            System.out.println("1 - Cadastrar Pessoa Física");
+            System.out.println("2 - Cadastrar Pessoa Jurídica");
+            System.out.println("3 - Imprimir Clientes");
+            System.out.println("4 - Sair");
+            opcao = tec.nextInt();
+            tec.nextLine();
+            switch (opcao) {
+                case 1:
+                    p[ultimo] = new Fisica();
+                    break;
+                case 2:
+                    p[ultimo] = new Juridica();
+                    break;
+                case 3:
+                    imprimeP();
+                    break;
+                case 4:
+                    System.out.println("Sair...");
+                    break;
+                default:
+                    System.out.println("Valor Inválido");
+                    break;
+            }
+            if(opcao == 1 || opcao == 2) {
+                p[ultimo].cadastra();
+                ultimo++;
+            }
+        }
+    }
+
+    private static void imprimeP() {
+        for(int i = 0; i < ultimo; i++) {
+            p[i].imprime();
+        }
     }
     
 }
